2f71b1b834b21b0004385cd51c6bdc0dbb67b2cf
AnnaBuller <aniabela@wp.pl>
2021-05-17 13:50:40 +0200

    use chooseTrips method

--- src/js/client.js
+++ src/js/client.js
@@ -3,19 +3,22 @@ import DOMHelper from './DOMHelper';
 import ExcursionsManager from './ExcursionsManager';
 
 document.addEventListener('DOMContentLoaded', init);
-const domHelper = new DOMHelper;
-const exManager = new ExcursionsManager;
 
 function init() {
+  const domHelper = new DOMHelper;
+  const exManager = new ExcursionsManager;
+
   const ulEl = domHelper.findElement('.panel__excursions');
   const prototypeLi = domHelper.findElement('.excursions__item--prototype');
+
   const ulElCart = domHelper.findElement('.panel__summary');
   const prototypeLiCart = domHelper.findElement('.summary__item--prototype');
+
   const elementsExist = domHelper.areElementsInDOM(ulEl, prototypeLi, ulElCart, prototypeLiCart)
 
   if (elementsExist) {
     exManager.showTrips(ulEl, prototypeLi);
-    ulEl.addEventListener('listLoaded', exManager.orderTrips(ulEl, ulElCart, prototypeLiCart))
+    ulEl.addEventListener('listLoaded', exManager.chooseTrips(ulEl, ulElCart, prototypeLiCart));
+    exManager.orderTrips()
   }
 }
-
